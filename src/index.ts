685dc59cd868c80ee74e68783e063abd39bd1a78
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2022-09-22 15:38:47 -0500

    Cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,32 @@
+import cors from 'cors';
 import express from 'express';
+
 import { routerApi } from './routes/';
 import {
   boomErrorHandler,
   errorHandler,
   logErrors,
 } from './middlewares/errorHandler';
+
 const app = express();
 
 // dotenv expected to define port
 const PORT = 5000;
 
-// Body parser
+//    Middlewares     //
+
+// body parser
 app.use(express.json());
+
+// Cors
+const whitelist = ['http://localhost:3000', 'http://localhost:5500'];
+const options = {
+  origin: (origin: string, callback: Function) => {
+    if (whitelist.includes(origin)) callback(null, true);
+    else callback(new Error('Not allowed'));
+  },
+};
+app.use(cors(options));
 
 // App routes
 routerApi(app);
